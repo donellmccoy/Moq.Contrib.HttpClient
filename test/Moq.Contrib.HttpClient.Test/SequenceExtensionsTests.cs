88e016a751c7967420c6cf90dc06ee030f1d0f7c
Donell McCoy <Donell_Mccoy@Dellteam.com>
2021-02-05 12:14:44 -0500

    Save point

--- test/Moq.Contrib.HttpClient.Test/SequenceExtensionsTests.cs
+++ test/Moq.Contrib.HttpClient.Test/SequenceExtensionsTests.cs
@@ -10,15 +10,14 @@ namespace Moq.Contrib.HttpClient.Test
 {
     public class SequenceExtensionsTests
     {
-        private readonly Mock<HttpMessageHandler> handler;
-        private readonly System.Net.Http.HttpClient client;
+        private readonly Mock<HttpMessageHandler> _handler;
+        private readonly System.Net.Http.HttpClient _client;
 
         public SequenceExtensionsTests()
         {
-            handler = new Mock<HttpMessageHandler>(MockBehavior.Strict);
-            client = handler.CreateClient();
-
-            client.BaseAddress = new Uri("https://example.com");
+            _handler = new Mock<HttpMessageHandler>(MockBehavior.Strict);
+            _client = _handler.CreateClient();
+            _client.BaseAddress = new Uri("https://example.com");
         }
 
         /**
@@ -31,33 +30,33 @@ namespace Moq.Contrib.HttpClient.Test
         [Fact]
         public async Task CanReturnSequenceOfResponsesForOneSetup()
         {
-            var url = new Uri(client.BaseAddress, "value");
+            var url = new Uri(_client.BaseAddress, "value");
 
             // Simulate a service outage to test retry logic
-            handler.SetupRequestSequence(url)
-                .ReturnsResponse(HttpStatusCode.ServiceUnavailable)
-                .ReturnsResponse(HttpStatusCode.ServiceUnavailable)
-                .ReturnsResponse(HttpStatusCode.ServiceUnavailable)
-                .ReturnsResponse("success");
+            _handler.SetupRequestSequence(url)
+                    .ReturnsResponse(HttpStatusCode.ServiceUnavailable)
+                    .ReturnsResponse(HttpStatusCode.ServiceUnavailable)
+                    .ReturnsResponse(HttpStatusCode.ServiceUnavailable)
+                    .ReturnsResponse("success");
 
             // Imaginary service method using Polly to handle retries
             // See https://github.com/App-vNext/Polly#polly for details
             async Task<string> GetValue()
             {
-                var result = await Policy
-                    .HandleResult<HttpResponseMessage>(r => r.StatusCode == HttpStatusCode.ServiceUnavailable)
-                    .RetryAsync(3)
-                    .ExecuteAsync(() => client.GetAsync("value"));
+                var result = await Policy.HandleResult<HttpResponseMessage>(r => r.StatusCode == HttpStatusCode.ServiceUnavailable)
+                                         .RetryAsync(3)
+                                         .ExecuteAsync(() => _client.GetAsync("value"));
 
                 result.EnsureSuccessStatusCode();
+
                 return await result.Content.ReadAsStringAsync();
             }
 
             // The method should try the request four times, receiving
             // a 503 the first three before it gets the success value
-            string value = await GetValue();
+            var value = await GetValue();
             value.Should().Be("success");
-            handler.VerifyRequest(url, Times.Exactly(4));
+            _handler.VerifyRequest(url, Times.Exactly(4));
         }
 
         [Theory]
@@ -73,29 +72,28 @@ namespace Moq.Contrib.HttpClient.Test
             // seventh, at which point he changes things up and says "chevron seven locked". Therefore we must be sure
             // to only "encode" six times and then finally "lock" only at the end of the sequence.
 
-            var encodeUrl = new Uri(client.BaseAddress, "chevrons/encode");
-            var lockUrl = new Uri(client.BaseAddress, "chevrons/lock");
+            var encodeUrl = new Uri(_client.BaseAddress, "chevrons/encode");
+            var lockUrl = new Uri(_client.BaseAddress, "chevrons/lock");
             var sequence = new MockSequence() { Cyclic = true };
 
             const string PointOfOrigin = "ᐰ";
 
-            for (int i = 1; i <= 6; i++)
+            for (var i = 1; i <= 6; i++)
             {
-                handler.InSequence(sequence)
-                    .SetupRequest(HttpMethod.Post, encodeUrl)
-                    .ReturnsResponse($"Chevron {i} encoded!");
+                _handler.InSequence(sequence)
+                        .SetupRequest(HttpMethod.Post, encodeUrl)
+                        .ReturnsResponse($"Chevron {i} encoded!");
             }
 
-            handler.InSequence(sequence)
-                .SetupRequest(HttpMethod.Post, lockUrl, async r =>
-                    (await r.Content.ReadAsStringAsync()) == PointOfOrigin)
-                .ReturnsResponse("Chevron 7, locked!");
+            _handler.InSequence(sequence)
+                    .SetupRequest(HttpMethod.Post, lockUrl, async r => (await r.Content.ReadAsStringAsync()) == PointOfOrigin)
+                    .ReturnsResponse("Chevron 7, locked!");
 
             async Task<string> PostAndReadString(Uri uri, string content = "")
             {
                 // Even though this is disposing of each response, on the second cycle it will get new response
                 // instances (another test of ResponseExtensionsTests.ReturnsNewResponseInstanceEachRequest)
-                using (var response = await client.PostAsync(uri, new StringContent(content)))
+                using (var response = await _client.PostAsync(uri, new StringContent(content)))
                 {
                     return await response.Content.ReadAsStringAsync();
                 }
